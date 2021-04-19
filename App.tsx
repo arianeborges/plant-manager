9b417c9371febf4e4c25310e0dd4e934e6eaa190
Ariane <ariianeboorges@gmail.com>
2021-04-19 15:54:43 +0100

    fix: Add new assets and change text of first view

--- App.tsx
+++ App.tsx
@@ -1,21 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app.</Text>
-      <StatusBar />
+    <View style={style.container}>
+      <Text>NLW#5</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
 });
