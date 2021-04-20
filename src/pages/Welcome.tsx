a82dfe56eff3334969e6435e1d2a14559f9e0d47
Ariane <ariianeboorges@gmail.com>
2021-04-20 14:23:20 +0100

    refact: Refact the Welcome page

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -13,20 +13,20 @@ import Icon from 'react-native-vector-icons/Feather';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        Gerencie {'\n'} suas plantas {'\n'} de forma fácil
+        Gerencie {'\n'} suas plantas de {'\n'} forma fácil
       </Text>
 
       <Image source={wateringImg} style={styles.image} resizeMode="contain" />
 
       <Text style={styles.subtitle}>
-        {' '}
-        Não esqueça mais de regar suas {'\n'} plantas. Nós cuidamos de lembrar
-        você {'\n'} sempre que precisar.
+        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+        sempre que precisar.
       </Text>
       <TouchableOpacity style={styles.button} activeOpacity={0.7}>
         <Icon name="chevron-right" size={32} style={styles.icon} />
@@ -41,18 +41,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-around',
     paddingTop: Platform.OS === 'android' ? 80 : 0,
+    paddingHorizontal: 20,
   },
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
   subtitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
   image: {
     height: Dimensions.get('window').width * 0.7,
