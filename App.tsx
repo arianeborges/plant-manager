1f74dcef38dbf0e7d267409e29f855761a290e41
Ariane <ariianeboorges@gmail.com>
2021-04-20 14:15:19 +0100

    feat: Install Jost fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import {
+  Jost_400Regular,
+  Jost_600SemiBold,
+  useFonts,
+} from '@expo-google-fonts/jost';
+import AppLoading from 'expo-app-loading';
+
 import { Welcome } from './src/pages/Welcome';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return <Welcome />;
 }
