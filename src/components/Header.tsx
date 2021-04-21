d5cc5656f7713cf0e45a6f796237d359b7a58025
Ariane <ariianeboorges@gmail.com>
2021-04-21 18:34:56 +0100

    fix: Add status bar height to the view

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import colors from '../styles/colors';
 
 export function Header() {
@@ -18,5 +19,6 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     paddingVertical: 20,
     backgroundColor: colors.red,
+    marginTop: getStatusBarHeight(),
   },
 });
