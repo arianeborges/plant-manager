5fd9d7503108f58f21703a2dde415e76c13cd3bc
Ariane <ariianeboorges@gmail.com>
2021-04-21 18:24:20 +0100

    feat: Add Plant Select page on routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -1,8 +1,11 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
+
 import { Confirmation } from '../pages/Confirmation';
+import { PlantSelect } from '../pages/PlantSelect';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Welcome } from '../pages/Welcome';
+
 import colors from '../styles/colors';
 
 const stackRoutes = createStackNavigator();
@@ -20,6 +23,8 @@ const AppRoutes: React.FC = () => (
     />
 
     <stackRoutes.Screen name="Confirmation" component={Confirmation} />
+
+    <stackRoutes.Screen name="PlantSelect" component={PlantSelect} />
   </stackRoutes.Navigator>
 );
 
