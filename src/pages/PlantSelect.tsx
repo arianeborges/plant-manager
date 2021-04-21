06af918f03483698d05cb589f775925d70da5711
Ariane <ariianeboorges@gmail.com>
2021-04-21 18:24:02 +0100

    feat: Create Plant Select page

---
+++ src/pages/PlantSelect.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import colors from '../styles/colors';
+
+export function PlantSelect() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.background,
+  },
+});
