5f89db62fdb34380162bb5f808fad1f8b48ec583
Ariane <ariianeboorges@gmail.com>
2021-04-21 18:57:51 +0100

    fix: Fix Header padding

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    padding: 20,
+    paddingVertical: 20,
     marginTop: getStatusBarHeight(),
   },
   greetings: {
