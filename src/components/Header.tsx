55c63586208271c787421b56cbbe083949b979cc
Ariane <ariianeboorges@gmail.com>
2021-04-21 18:51:42 +0100

    feat: Add some style to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Header() {
   return (
     <View style={styles.container}>
-      <Text>Hello, Fulano</Text>
+      <View>
+        <Text style={styles.greetings}>Hello,</Text>
+        <Text style={styles.userName}>Fulano</Text>
+      </View>
+
+      <Image
+        source={{
+          uri:
+            'https://pyxis.nymag.com/v1/imgs/a22/179/b205a78510315d932427143ad6b6fb5694-03-homer-simpson-doughnuts.rsquare.w700.jpg',
+        }}
+        style={styles.image}
+      />
     </View>
   );
 }
@@ -17,8 +29,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flexDirection: 'row',
     justifyContent: 'space-between',
-    paddingVertical: 20,
-    backgroundColor: colors.red,
+    padding: 20,
     marginTop: getStatusBarHeight(),
+  },
+  greetings: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
   },
 });
