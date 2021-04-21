0eef866f0be6298f617dfa25e5e7c2bb58e01d41
Ariane <ariianeboorges@gmail.com>
2021-04-21 20:55:34 +0100

    feat: Apply fetch data from  server

--- src/pages/PlantSelect.tsx
+++ src/pages/PlantSelect.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import { Header } from '../components/Header';
 import { RoomButton } from '../components/RoomButton';
+
+import api from '../services/api';
+
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
+interface RoomProps {
+  key: string;
+  title: string;
+}
+
 export function PlantSelect() {
+  const [rooms, setRooms] = useState<RoomProps[]>([]);
+
+  useEffect(() => {
+    async function fetchRoom() {
+      const { data } = await api.get('plants_environments');
+      setRooms(data);
+    }
+    fetchRoom();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -14,7 +33,19 @@ export function PlantSelect() {
         <Text style={styles.title}>In which room </Text>
         <Text style={styles.subtitle}>do you want to place your plant?</Text>
       </View>
-      <RoomButton title="Kitchen" active />
+      <View>
+        <FlatList
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={styles.roomList}
+          data={rooms}
+          renderItem={({ item }) => (
+            <>
+              <RoomButton title={item.title} active />
+            </>
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -39,5 +70,12 @@ const styles = StyleSheet.create({
     fontFamily: fonts.text,
     color: colors.heading,
     lineHeight: 20,
+  },
+  roomList: {
+    height: 40,
+    justifyContent: 'center',
+    paddingBottom: 5,
+    marginLeft: 32,
+    marginVertical: 32,
   },
 });
