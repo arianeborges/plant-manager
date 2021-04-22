eb18e030b72f5c7e1824ab6a04e42cbc260d28fd
Ariane <ariianeboorges@gmail.com>
2021-04-22 20:50:49 +0100

    refact: Refact load plants from storage

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -1,4 +1,5 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { format } from 'date-fns';
 
 export interface PlantProps {
   id: string;
@@ -14,14 +15,14 @@ export interface PlantProps {
   dateTimeNotification: Date;
 }
 
-interface StoragePlantPros {
+interface StoragePlantProps {
   [id: string]: { data: PlantProps };
 }
 
 export async function savePlant(plant: PlantProps): Promise<void> {
   try {
     const data = await AsyncStorage.getItem('@plantmanager:plants');
-    const oldPlants = data ? (JSON.parse(data) as StoragePlantPros) : {};
+    const oldPlants = data ? (JSON.parse(data) as StoragePlantProps) : {};
 
     const newPlant = {
       [plant.id]: {
@@ -38,12 +39,29 @@ export async function savePlant(plant: PlantProps): Promise<void> {
   }
 }
 
-export async function loadPlant(): Promise<StoragePlantPros> {
+export async function loadPlant(): Promise<PlantProps[]> {
   try {
     const data = await AsyncStorage.getItem('@plantmanager:plants');
-    const plants = data ? (JSON.parse(data) as StoragePlantPros) : {};
+    const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
 
-    return plants;
+    const plantsSorted = Object.keys(plants)
+      .map(plant => {
+        return {
+          ...plants[plant].data,
+          hour: format(
+            new Date(plants[plant].data.dateTimeNotification),
+            'HH:mm',
+          ),
+        };
+      })
+      .sort((a, b) =>
+        Math.floor(
+          new Date(a.dateTimeNotification).getTime() / 1000 -
+            Math.floor(new Date(b.dateTimeNotification).getTime() / 1000),
+        ),
+      );
+
+    return plantsSorted;
   } catch (error) {
     throw new Error(error);
   }
