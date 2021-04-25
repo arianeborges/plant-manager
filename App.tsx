28d6dab38f99a3bdcef6f165a62f561857726279
Ariane <ariianeboorges@gmail.com>
2021-04-25 17:02:05 +0100

    feat: Handle notifications

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import * as Notifications from 'expo-notifications';
 import {
   Jost_400Regular,
   Jost_600SemiBold,
@@ -7,12 +8,43 @@ import {
 import AppLoading from 'expo-app-loading';
 
 import Routes from './src/routes';
+import { PlantProps } from './src/libs/storage';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold,
   });
+
+  useEffect(() => {
+    Notifications.setNotificationHandler({
+      handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+      }),
+    });
+
+    // you can see all the notifications
+    const subscription = Notifications.addNotificationReceivedListener(
+      async notification => {
+        const data = notification.request.content.data.plant as PlantProps;
+        console.log(data);
+      },
+    );
+
+    return () => subscription.remove();
+
+    // async function notifications() {
+    //   // see all notifications
+    //   // const data = Notifications.getAllScheduledNotificationsAsync();
+    //   // console.log(data);
+
+    //   // cancel all notifications
+    //   await Notifications.cancelAllScheduledNotificationsAsync();
+    // }
+    // notifications();
+  }, []);
 
   if (!fontsLoaded) {
     return <AppLoading />;
